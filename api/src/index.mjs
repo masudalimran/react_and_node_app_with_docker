36e413523d81ee6009222faeb873569dbcfd396e
Masud Al Imran <masudalimran92@gmail.com>
2023-12-28 17:17:38 +0600

    first commit

---
+++ api/src/index.mjs
@@ -0,0 +1,45 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const data = [
+    {
+      name: "Faruk",
+      age: 23,
+    },
+    {
+      name: "Raju",
+      age: 26,
+    },
+    {
+      name: "Rijon",
+      age: 35,
+    },
+    {
+      name: "Sami",
+      age: 16,
+    },
+    {
+      name: "Sajib",
+      age: 20,
+    },
+    {
+      name: "Imon",
+      age: 28,
+    },
+    {
+      name: "Sabbir",
+      age: 19,
+    },
+    {
+      name: "Sakib",
+      age: 19,
+    },
+  ];
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
